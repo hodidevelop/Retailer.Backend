cc5ba00750cc7cbca40a99662589133e75d757c8
Hodvogner Zoltán <hodvogner.zoltan@autsoft.hu>
2022-02-25 13:27:17 +0100

    Migrations has been added to DALs

---
+++ Retailer.Backend.Dal.InvoiceService/Migrations/20220225121936_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Retailer.Backend.Dal.InvoiceService.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    State = table.Column<int>(type: "INTEGER", nullable: false),
+                    BillingAddress = table.Column<string>(type: "TEXT", maxLength: 255, nullable: true),
+                    Amount = table.Column<double>(type: "REAL", nullable: false),
+                    AmountPaid = table.Column<double>(type: "REAL", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Invoices");
+        }
+    }
+}
---
+++ Retailer.Backend.Dal.OrderService/Migrations/20220225122454_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Retailer.Backend.Dal.OrderService.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    State = table.Column<int>(type: "INTEGER", nullable: false),
+                    CustomerName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    DeliveryAddress = table.Column<string>(type: "TEXT", maxLength: 255, nullable: true),
+                    ProductName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    UnitPrice = table.Column<double>(type: "REAL", nullable: false),
+                    Quantity = table.Column<double>(type: "REAL", nullable: false),
+                    InvoiceId = table.Column<int>(type: "INTEGER", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
